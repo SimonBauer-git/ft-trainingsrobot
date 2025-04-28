36bba304d3a1f9256e3eabdefb322a6ca6e13295
Simon Bauer <87330127+SimonBauer-git@users.noreply.github.com>
2025-04-28 13:12:21 +0200

    Add files via upload
    steuerung mittels potentiometer als (absolut-)encoder

---
+++ trainingsroboter_steuerung_parallel_poti/trainingsroboter_steuerung_parallel_poti.ino
@@ -0,0 +1,31 @@
+//programm zur steuerung meines trainingsroboters mit poti
+
+#include <AFMotor.h>
+#include <Servo.h>
+
+char SerialVal;
+const int close = 180;
+const int open = 90;
+
+Servo tool;        //servo intitialisieren
+AF_DCMotor a1(4);  //motor 1 = linker motor, letzte achse von unten gesehen
+AF_DCMotor a2(2);  //motor 2 = rechter motor, untere achse des arms
+AF_DCMotor a3(1);  //motor 3 (4) = drehachse unten
+
+void setup() {
+  Serial.begin(115200);  //Seriellle schnittstelle starten
+  a1.setSpeed(255);
+  a2.setSpeed(127);
+  a3.setSpeed(127);
+  tool.attach(9);
+  tool.write(open);
+  homing();
+  
+}
+
+void loop() {
+  moverobot(-5, 10, -5);
+  delay(2000);
+  moverobot(0, 0, 0);
+  delay(2000);
+}
