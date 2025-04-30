a85cc680bb32711fd4dcdd257ed2b0d81a08a915
Simon Bauer <87330127+SimonBauer-git@users.noreply.github.com>
2025-04-30 21:33:59 +0200

    Update motor_ctrl.ino

--- trainingsroboter_steuerung_parallel_poti/motor_ctrl.ino
+++ trainingsroboter_steuerung_parallel_poti/motor_ctrl.ino
@@ -2,12 +2,17 @@ bool reached_a1, reached_a2, reached_a3;
 int demand_a1, demand_a2, demand_a3, pos_a1, pos_a2, pos_a3;
 
 void homing() {  //homing sequence
-moverobot(0,0,0);
+moverobot(950,730,800);
+delay(2000);
 }
 
 
 
 void moverobot(int demand_a1, int demand_a2, int demand_a3) {
+  demand_a1=constrain(demand_a1, 580, 1000);
+  demand_a2=constrain(demand_a2, 730, 1000);
+  demand_a3=constrain(demand_a3, 800, 1000);
+
 while(reached_a1 == false || reached_a2 == false || reached_a3 == false){
   if (reached_a1 == false) {
     pos_a1=analogRead(A7);
@@ -60,7 +65,11 @@ while(reached_a1 == false || reached_a2 == false || reached_a3 == false){
       reached_a3 = true;
     }
     }
-}
+Serial.print(pos_a1);
+Serial.print(",");
+Serial.print(pos_a2);
+Serial.print(",");
+Serial.println(pos_a3);}
 //____________________________________________________//
   
   //flags resetten
