d9b232e8596f259c727fb975c2e9cce8447b5b88
Simon Bauer <87330127+SimonBauer-git@users.noreply.github.com>
2025-05-20 11:22:23 +0200

    Update motor_ctrl.ino

--- trainingsroboter_steuerung_parallel_poti_serial/motor_ctrl.ino
+++ trainingsroboter_steuerung_parallel_poti_serial/motor_ctrl.ino
@@ -1,6 +1,8 @@
+
+
 bool reached_a1, reached_a2, reached_a3;
 int demand_a1, demand_a2, demand_a3, pos_a1, pos_a2, pos_a3;
-long unsigned int pm = 0;
+
 
 void homing() {  //homing sequence
   moverobot(990, 730, 130);
@@ -80,86 +82,5 @@ void moverobot(int demand_a1, int demand_a2, int demand_a3) {
   reached_a3 = false;
 }
 
-void manuell() {
-  a2.setSpeed(125);
-  a3.setSpeed(80);
-  if (Serial.available()) {
-    SerialVal = Serial.read();
-  }
 
-  switch (SerialVal) {
-    case 'w':
-      a2.run(FORWARD);
-      break;
-    case 's':
-      a1.run(RELEASE);
-      a2.run(RELEASE);
-      a3.run(RELEASE);
-      break;
-    case 'k':
-      a1.run(RELEASE);
-      a2.run(RELEASE);
-      a3.run(RELEASE);
-      break;
-    case 'x':
-      a2.run(BACKWARD);
-      break;
-    case 'a':
-      a1.run(FORWARD);
-      break;
-    case 'd':
-      a1.run(BACKWARD);
-      break;
-    case 'i':
-      a3.run(BACKWARD);
-      break;
-    case ',':
-      a3.run(FORWARD);
-      break;
-    case 'h':
-      homing();
-      break;
-    case 'p':
-      automatik();
-      break;
-  }
-  if (millis() - pm > 200) {
-    Serial.print(analogRead(A7));
-    Serial.print(",");
-    Serial.print(analogRead(A8));
-    Serial.print(",");
-    Serial.println(analogRead(A9));
-    pm = millis();
-  }
-}
 
-void automatik() {
-  a2.setSpeed(255);
-  a3.setSpeed(255);
-  bool e1 = false;
-  while (e1 == false) {
-
-moverobot(500,850,300);
-delay(2000);
-moverobot(990,730,130);
-delay(2000);
-
-    if (Serial.available()) {
-      SerialVal = Serial.read();
-    }
-
-    if (SerialVal == 's' || SerialVal == 'k') { e1 = true; }
-    if (millis() - pm > 200) {
-      Serial.print("Automatikmodus, ");
-      Serial.print(analogRead(A7));
-      Serial.print(",");
-      Serial.print(analogRead(A8));
-      Serial.print(",");
-      Serial.println(analogRead(A9));
-      pm = millis();
-    }
-  }
-  a1.run(RELEASE);
-  a2.run(RELEASE);
-  a3.run(RELEASE);
-}
