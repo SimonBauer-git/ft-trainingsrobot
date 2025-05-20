ed69c38ac87bf77ebcebc329f6c2fda26e28bdfa
Simon Bauer <87330127+SimonBauer-git@users.noreply.github.com>
2025-05-20 19:41:37 +0200

    Update automatik.ino

--- trainingsroboter_steuerung_basisprogramm/automatik.ino
+++ trainingsroboter_steuerung_basisprogramm/automatik.ino
@@ -1,87 +1,18 @@
-//dieser teil des code ist modular auswechselbar
+//dieser teil des codes ist modular auswechselbar
 void programm() {
   automatik();
 }
 
-void manuell() {
-  Serial.println("Manuelle Steuerung")
-  delay(500);
-  a2.setSpeed(125);
-  a3.setSpeed(80);
-  if (Serial.available()) {
-    SerialVal = Serial.read();
-  }
-
-  switch (SerialVal) {
-    case 'w':
-      a2.run(FORWARD);
-      break;
-    case 's':
-      a1.run(RELEASE);
-      a2.run(RELEASE);
-      a3.run(RELEASE);
-      break;
-    case 'k':
-      a1.run(RELEASE);
-      a2.run(RELEASE);
-      a3.run(RELEASE);
-      break;
-    case 'x':
-      a2.run(BACKWARD);
-      break;
-    case 'a':
-      a1.run(FORWARD);
-      break;
-    case 'd':
-      a1.run(BACKWARD);
-      break;
-    case 'i':
-      a3.run(BACKWARD);
-      break;
-    case ',':
-      a3.run(FORWARD);
-      break;
-    case 'h':
-      homing();
-      break;
-    case 'p':
-      automatik();
-      break;
-  }
-  if (millis() - pm > 200) {
-    Serial.print("J1=");
-    Serial.print(analogRead(A7));
-    Serial.print(",");
-    Serial.print(" J2=");
-    Serial.print(analogRead(A8));
-    Serial.print(",");
-    Serial.print(" J3=");
-    Serial.println(analogRead(A9));
-    pm = millis();
-  }
-}
-
-
 void automatik() {
   Serial.println("Automatik, s oder k zum beenden");
+  delay(500);
   a2.setSpeed(255);
   a3.setSpeed(255);
-  bool e1 = false;
-  while (e1 == false) {
+  while (true) {
 
     moverobot(500, 850, 300);
     delay(2000);
     moverobot(990, 730, 130);
     delay(2000);
-
-    if (Serial.available()) {
-      SerialVal = Serial.read();
-    }
-
-    if (SerialVal == 's' || SerialVal == 'k') { e1 = true; }
   }
-}
-a1.run(RELEASE);
-a2.run(RELEASE);
-a3.run(RELEASE);
 }
